42f09f399a0a7275b04ee4763d34975085aaa477
Peter Borocz <680893+PBorocz@users.noreply.github.com>
2023-02-06 15:31:16 -0800

    Add ability (and test) for update_readme to work on markdown as well.

--- manage/steps/update_readme.py
+++ manage/steps/update_readme.py
@@ -33,28 +33,50 @@ def main(configuration: Configuration, recipes: Recipes, step: dict) -> bool:
     ...
 
     """
+    # What's the default path we'll be working on? (note: this is primarily for testing purposes), manage is intended to be run
+    # from the project's top level directory)
+    cwd = Path(step.arguments.get('cwd', Path.cwd()))
+
+    # What file format are we working on?
+    format_ = step.arguments.get("readme_format", "md")  # Default README file format if not specified.
+
+    # What does the "Unreleased" line look like based on the format?
+    if format_ == "md":
+        header = "###"
+    elif format_ == "org":
+        header = "***"
+    else:
+        print(f"[red]Sorry, we don't support readme_format {format_} yet, must be one of 'md' or 'org'!")
+        return False
+
+    unreleased_header = f"{header} Unreleased"
+
+    # README file name and path..
+    readme_name = f"README.{format_}"
+    path_readme = cwd / readme_name
+
     if step.confirm:
-        msg = f"Ok to update README.org's 'Unreleased' header to {configuration.version()}?"
+        msg = f"Ok to update {readme_name}'s 'Unreleased' header to {configuration.version()}?"
         if not ask_confirm(msg):
             print("Nothing done (but pyproject.toml may still be on new version)")
             return False
 
-    # Read and update the Changelog section embedded our README.org with the
-    # new version (leaving another "Unreleased" header for future work)
-    readme = PATH_README.read_text()
-    if UNRELEASED_HEADER not in readme:
+    # Read and update the Changelog section embedded in our readme with the new version (leaving another "Unreleased" header for
+    # future work)
+    readme_contents = path_readme.read_text()
+    if unreleased_header not in readme_contents:
         failure()
-        print(f"[red]Sorry, couldn't find a header consisting of '{UNRELEASED_HEADER}' in README.org!")
+        print(f"[red]Sorry, couldn't find a header consisting of '{unreleased_header}' in {readme_name}!")
         return False
 
-    msg = fmt("Running update to README.org: '{UNRELEASED_HEADER}'", color='blue')
+    msg = fmt(f"Running update to {readme_name}: '{unreleased_header}'", color='blue')
     print(msg, flush=True, end="")
-    release_header = f"\n*** {configuration.version()} - {datetime.now().strftime('%Y-%m-%d')}"
-    readme = readme.replace(
-        UNRELEASED_HEADER,
-        UNRELEASED_HEADER + release_header)
+    release_header = f"\n{header} {configuration.version()} - {datetime.now().strftime('%Y-%m-%d')}"
+    readme_contents = readme_contents.replace(
+        unreleased_header,
+        unreleased_header + "\n" + release_header)
 
-    PATH_README.write_text(readme)
+    path_readme.write_text(readme_contents)
 
     success()
     return True
---
+++ tests/steps/test_update_readme.py
@@ -0,0 +1,81 @@
+"""Command test."""
+from pathlib import Path
+
+import pytest
+
+from manage.models import Configuration, Recipes, Step
+from manage.steps.update_readme import main as update_readme
+
+
+test_readme_md = """
+## Release History
+
+### Unreleased
+   - [[https://towardsdatascience.com/should-we-use-custom-exceptions-in-python-b4b4bca474ac][custom exceptions]]
+
+### v1.9.10
+   - open text open text open text open text open text open text open text open text open text open text open text.
+
+### v1.9.09
+   - open text open text open text open text open text open text open text open text open text open text open text.
+"""
+
+
+test_readme_org = """
+** Release History
+
+*** Unreleased
+   - [[https://towardsdatascience.com/should-we-use-custom-exceptions-in-python-b4b4bca474ac][custom exceptions]]
+
+*** v1.9.10
+   - open text open text open text open text open text open text open text open text open text open text open text.
+
+*** v1.9.09
+   - open text open text open text open text open text open text open text open text open text open text open text.
+"""
+
+@pytest.fixture
+def path_readme_md():
+    path_ = Path("/tmp/README.md")
+    path_.write_text(test_readme_md)
+    yield path_
+    if path_.exists():
+        path_.unlink()
+
+@pytest.fixture
+def path_readme_org():
+    path_ = Path("/tmp/README.org")
+    path_.write_text(test_readme_org)
+    yield path_
+    if path_.exists():
+        path_.unlink()
+
+
+def test_md(path_readme_md):
+    # Setup
+    step = Step(method="aMethod", confirm=False, quiet_mode=True, arguments=dict(readme_format="md", cwd="/tmp"))
+
+    # Test
+    assert update_readme(Configuration(version_="1.9.11"), Recipes.parse_obj({}), step)
+
+    # Confirm
+    assert path_readme_md.exists()
+
+    readme = path_readme_md.read_text()
+    assert "### Unreleased" in readme
+    assert "v1.9.11" in readme
+
+
+def test_org(path_readme_org):
+    # Setup
+    step = Step(method="aMethod", confirm=False, quiet_mode=True, arguments=dict(readme_format="org", cwd="/tmp"))
+
+    # Test
+    assert update_readme(Configuration(version_="1.9.11"), Recipes.parse_obj({}), step)
+
+    # Confirm
+    assert path_readme_org.exists()
+
+    readme = path_readme_org.read_text()
+    assert "*** Unreleased" in readme
+    assert "v1.9.11" in readme
