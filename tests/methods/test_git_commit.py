ea77bbb06295a85507697a6a599f8179afca46d7
Peter Borocz <680893+PBorocz@users.noreply.github.com>
2023-07-19 16:45:35 -0700

    Commit as of 2023-07-19T16:39:29.636033

--- tests/methods/test_git_commit.py
+++ tests/methods/test_git_commit.py
@@ -12,20 +12,22 @@ def test_git_commit(git_repo):
     file_path.write_text("Initial commit")
     repo.index.add([file_path])
 
-    # Test
+    # Test!
     step = Step(method="aMethod", confirm=False, quiet_mode=False, arguments=dict(message=COMMIT_MESSAGE))
     assert git_commit(Configuration(), Recipes.parse_obj({}), step, repo=repo)
 
     # Confirm: 1 - Did we get a commit?
     try:
-        commits = list(repo.iter_commits())
-        assert len(commits) == 1, "Sorry, we didn't find exactly 1 commit on our test repo."
+        commit = repo.head.commit
     except ValueError:
         assert False, "Sorry, we didn't find anything committed to our test repo."
 
-    # Confirm: 2 - Does it have the right message?
-    assert commits[0].message == COMMIT_MESSAGE, "Sorry, we didn't find the right commit message on our commit"
+    # Confirm: 2 - Was it only of a single file?
+    assert commit.stats.total.get('files') == 1
 
-    # Confirm: 3 - Does it have the right file?
-    tree = commits[0].tree
-    assert tree[0].name == file_path.name, f"Sorry, unable to find {file_path.name=} in commit."
+    # Confirm: 3 - Was it the *right* file?
+    for file, diff in commit.stats.files.items():
+        assert file == file_path.name, f"Sorry, unable to find {file_path.name=} in commit.stats!"
+
+    # Confirm: 4 - Does it have the right message?
+    assert commit.message == COMMIT_MESSAGE, "Sorry, we didn't find the right commit message on our commit"
